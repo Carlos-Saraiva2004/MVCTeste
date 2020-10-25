8b848f54c6f74793a64df30d051f997841447503
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2020-10-25 10:30:20 +0000

    Project created

---
+++ MVCTeste/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCTeste.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
